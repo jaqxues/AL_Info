f9aaa59d3d31cf6ce38979797eac9c1db8d77d3d
jaqxues <32979131+jaqxues@users.noreply.github.com>
2019-09-23 23:23:51 +0200

    Initial commit

---
+++ 1_Introduction/5/Ex1_1.py
@@ -0,0 +1,20 @@
+# Exercise in Python File format
+
+# 1.
+print(27 + 15)
+print(26 - 18)
+print(6 * 7)
+print(1024 / 512)
+print(247 // 60)
+print(247 % 60)
+
+# 2.
+a = 2
+print(a)
+b = 9
+print(b)
+print(a + b)
+print(a * b)
+c = 42 * a
+print(c)
+print(c + b)
---
+++ 1_Introduction/5/Ex1_2.py
@@ -0,0 +1,12 @@
+s1 = "Hello"
+s2 = "world!"
+print(s1, s2)
+a = 2
+b = 42
+print(a+b)
+name = input("Enter your name: ")
+print("Hello", name)
+
+number1 = int(input("Enter a first number: "))
+number2 = int(input("Enter a second number: "))
+print(number1 * number2)
