dd5a1bda8555e7f227602cdf61c61bafd6132400
jaqxues <32979131+jaqxues@users.noreply.github.com>
2019-10-02 14:38:33 +0200

    Is Prime Number without loop / recursive check

---
+++ 3_StructureAlternative/Ex3_10.py
@@ -0,0 +1,7 @@
+n = int(input("Prime Number: "))
+
+if (n % 2 == 0 or n % 3 == 0 or n % 5 == 0 or n % 7 == 0) and (n != 2 and n != 3 and n != 5 and n != 7) or (
+        n == 0 or n == 1):
+    print(n, "is not a prime number")
+else:
+    print(n, "is a prime number")
